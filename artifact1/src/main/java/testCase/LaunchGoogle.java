8b6bb0ac40fb10b62f8f2eacb3a10f0fbed56879
bhdharan <bhdharan@PW7AM1XDH000204.r1-core.r1.aig.net>
2017-12-04 13:14:58 +0530

    Checking email

--- artifact1/src/main/java/testCase/LaunchGoogle.java
+++ artifact1/src/main/java/testCase/LaunchGoogle.java
@@ -11,6 +11,7 @@ public class LaunchGoogle extends ProjectWrappers {
 	public LaunchGoogle(RemoteWebDriver dr, ExtentTest Frt) {
 		super(dr, Frt);
 		this.driver=driver;
+		this.Frt=Frt;
 	
 		// TODO Auto-generated constructor stub
 	}
