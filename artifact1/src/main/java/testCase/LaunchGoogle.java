1214aacce6debc8ee2b2e0a515acec1389b707ed
bhdharan <bhdharan@PW7AM1XDH000204.r1-core.r1.aig.net>
2017-12-04 11:37:41 +0530

    Add testCase

---
+++ artifact1/src/main/java/testCase/LaunchGoogle.java
@@ -0,0 +1,18 @@
+package testCase;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.relevantcodes.extentreports.ExtentTest;
+
+import wrappers.ProjectWrappers;
+
+public class LaunchGoogle extends ProjectWrappers {
+
+	public LaunchGoogle(RemoteWebDriver driver, ExtentTest test) {
+		super(driver, test);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
